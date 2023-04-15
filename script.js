deb4a3f37c6f9b33c16d8bee678242fc001847e8
Sudip kumar <92117746+ksudip@users.noreply.github.com>
2023-04-15 13:28:23 +0530

    portfolio commit
    this is my profile.

---
+++ script.js
@@ -0,0 +1,14 @@
+// typing animation
+
+var typed =new Typed('.typing', {
+    strings:[
+        "",
+        'Complete React',
+        'Learn backend',
+        'Start leetcode',
+        'Read courses'
+    ],
+    typeSpeed: 200,
+    backSpeed:60,
+    loop: true
+})
